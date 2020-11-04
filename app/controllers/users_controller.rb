988b58ca3cece74323c0ede679badb44f6066466
yani82 <yani.iman1@gmail.com>
2020-11-04 01:01:54 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,74 @@
 class UsersController < ApplicationController
 
+    get '/signup' do 
+        if is_logged_in? 
+          redirect '/tweets' 
+        else 
+          erb :'users/create_user'
+        end 
+    end 
+
+    get "/users/:slug" do 
+        @user = User.find_by_slug(params[:slug])
+        erb :'/users/show'
+    end 
+
+    post '/signup' do 
+        user = User.new(params)
+        if user.save && user.username != "" && user.email != ""
+          session[:user_id] = user.id 
+
+        # or if params[:username].empty? || params[:email].empty? || params[:password].empty?
+    
+          redirect to '/signup'
+        end 
+        
+        if @user.save 
+            session[:user_id] = @user.id 
+            redirect '/tweets' 
+        end 
+    end 
+
+    get '/login' do 
+        if logged_in?
+          redirect '/tweets'
+        else 
+          erb :'/users/login'
+        end 
+    end 
+
+    get '/logout' do
+        if logged_in?
+            session.clear 
+    
+            redirect '/login' 
+        else
+            redirect '/' 
+        end 
+    end
+    
+    post '/login' do 
+        user = User.find_by(:username => params[:username])
+        # @user? 
+    
+        if user && user.autenticate(params[:password])
+            session[:user_id] = user.id
+            redirect '/tweets' 
+        else 
+            redirect '/login' 
+        end 
+      end 
 
 end
+
+
+
+
+  
+
+
+
+
+  
+
+  
