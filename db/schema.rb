9b6d120f423204434e83de9fe037059af9f6995d
Yani Asari <yani.iman1@gmail.com>
2020-11-06 06:11:32 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_11_06_060535) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+  end
 
 end
