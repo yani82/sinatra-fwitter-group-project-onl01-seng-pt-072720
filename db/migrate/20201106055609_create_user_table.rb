5e57bad031d1e4a7ef7c476f7280aac4256f0431
Yani Asari <yani.iman1@gmail.com>
2020-11-09 12:23:40 +0000

    Automatically backed up by Learn

--- db/migrate/20201106055609_create_user_table.rb
+++ db/migrate/20201106055609_create_user_table.rb
@@ -1,9 +1,12 @@
 class CreateUserTable < ActiveRecord::Migration[6.0]
   def change
+<<<<<<< HEAD
     create_table :users do |t|
       t.string :username
       t.string :email
       t.string :password
     end
+=======
+>>>>>>> de2eb7cf39460ae172b0a2bedcf9f604cc759002
   end
 end
--- db/migrate/20201106055650_create_tweeter_table.rb
+++ db/migrate/20201106055650_create_tweeter_table.rb
@@ -1,8 +1,11 @@
 class CreateTweeterTable < ActiveRecord::Migration[6.0]
   def change
+<<<<<<< HEAD
     create_table :tweets do |t|
       t.string :content
       t.integer :user_id
     end
+=======
+>>>>>>> de2eb7cf39460ae172b0a2bedcf9f604cc759002
   end
 end
